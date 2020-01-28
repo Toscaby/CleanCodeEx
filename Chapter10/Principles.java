879c32d5dba919328cad55143b582eb80e37a9da
liz <lizbyu@gmail.com>
2020-01-28 20:13:57 +0800

    feat: Chapter10 principles

---
+++ Chapter10/Principles.java
@@ -0,0 +1,16 @@
+package Chapter10;
+
+/**
+ * @author Tosca
+ * @date 28/1/2020
+ */
+public class Principles {
+  // A. 类的组织
+  // 1) 变量--> 公共函数
+  // 2) 变量顺序: psf --> prsf --> prs--> pr
+  // 3) 把公共函数调用的私有函数紧随在公共函数后，top-down
+
+  // B.类应该短小 类衡量的原则是：类的权责
+  // 1) SRP 单一权责:类或模块只有一条加以修改的理由
+  // 2) 高内聚 （低耦合
+}
