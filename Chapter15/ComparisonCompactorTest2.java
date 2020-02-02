3e9427e4d036778782ef525aca1eae9cbcbd6dc8
liz <lizbyu@gmail.com>
2020-02-02 19:14:49 +0800

    chores: cp original ComparisonCompactorTest from Junit4 source code

---
+++ Chapter15/ComparisonCompactorTest2.java
@@ -0,0 +1,108 @@
+package Chapter15;
+
+import Chapter15.finalversion.ComparisonCompactor;
+import junit.framework.TestCase;
+
+/**
+ * copy from junit4-r4.13/src/test/java/junit/tests/framework
+ * @author Tosca
+ * @date 2/2/2020
+ */
+public class ComparisonCompactorTest2 extends TestCase {
+
+    public void testMessage() {
+      String failure = new ComparisonCompactor(0, "b", "c").formatCompactedComparison("a");
+      assertTrue("a expected:<[b]> but was:<[c]>".equals(failure));
+    }
+
+    public void testStartSame() {
+      String failure = new ComparisonCompactor(1, "ba", "bc").formatCompactedComparison(null);
+      assertEquals("expected:<b[a]> but was:<b[c]>", failure);
+    }
+
+    public void testEndSame() {
+      String failure = new ComparisonCompactor(1, "ab", "cb").formatCompactedComparison(null);
+      assertEquals("expected:<[a]b> but was:<[c]b>", failure);
+    }
+
+    public void testSame() {
+      String failure = new ComparisonCompactor(1, "ab", "ab").formatCompactedComparison(null);
+      assertEquals("expected:<ab> but was:<ab>", failure);
+    }
+
+    public void testNoContextStartAndEndSame() {
+      String failure = new ComparisonCompactor(0, "abc", "adc").formatCompactedComparison(null);
+      assertEquals("expected:<...[b]...> but was:<...[d]...>", failure);
+    }
+
+    public void testStartAndEndContext() {
+      String failure = new ComparisonCompactor(1, "abc", "adc").formatCompactedComparison(null);
+      assertEquals("expected:<a[b]c> but was:<a[d]c>", failure);
+    }
+
+    public void testStartAndEndContextWithEllipses() {
+      String failure = new ComparisonCompactor(1, "abcde", "abfde").formatCompactedComparison(null);
+      assertEquals("expected:<...b[c]d...> but was:<...b[f]d...>", failure);
+    }
+
+    public void testComparisonErrorStartSameComplete() {
+      String failure = new ComparisonCompactor(2, "ab", "abc").formatCompactedComparison(null);
+      assertEquals("expected:<ab[]> but was:<ab[c]>", failure);
+    }
+
+    public void testComparisonErrorEndSameComplete() {
+      String failure = new ComparisonCompactor(0, "bc", "abc").formatCompactedComparison(null);
+      assertEquals("expected:<[]...> but was:<[a]...>", failure);
+    }
+
+    public void testComparisonErrorEndSameCompleteContext() {
+      String failure = new ComparisonCompactor(2, "bc", "abc").formatCompactedComparison(null);
+      assertEquals("expected:<[]bc> but was:<[a]bc>", failure);
+    }
+
+    public void testComparisonErrorOverlappingMatches() {
+      String failure = new ComparisonCompactor(0, "abc", "abbc").formatCompactedComparison(null);
+      assertEquals("expected:<...[]...> but was:<...[b]...>", failure);
+    }
+
+    public void testComparisonErrorOverlappingMatchesContext() {
+      String failure = new ComparisonCompactor(2, "abc", "abbc").formatCompactedComparison(null);
+      assertEquals("expected:<ab[]c> but was:<ab[b]c>", failure);
+    }
+
+    public void testComparisonErrorOverlappingMatches2() {
+      String failure = new ComparisonCompactor(0, "abcdde", "abcde").formatCompactedComparison(null);
+      assertEquals("expected:<...[d]...> but was:<...[]...>", failure);
+    }
+
+    public void testComparisonErrorOverlappingMatches2Context() {
+      String failure = new ComparisonCompactor(2, "abcdde", "abcde").formatCompactedComparison(null);
+      assertEquals("expected:<...cd[d]e> but was:<...cd[]e>", failure);
+    }
+
+    public void testComparisonErrorWithActualNull() {
+      String failure = new ComparisonCompactor(0, "a", null).formatCompactedComparison(null);
+      assertEquals("expected:<a> but was:<null>", failure);
+    }
+
+    public void testComparisonErrorWithActualNullContext() {
+      String failure = new ComparisonCompactor(2, "a", null).formatCompactedComparison(null);
+      assertEquals("expected:<a> but was:<null>", failure);
+    }
+
+    public void testComparisonErrorWithExpectedNull() {
+      String failure = new ComparisonCompactor(0, null, "a").formatCompactedComparison(null);
+      assertEquals("expected:<null> but was:<a>", failure);
+    }
+
+    public void testComparisonErrorWithExpectedNullContext() {
+      String failure = new ComparisonCompactor(2, null, "a").formatCompactedComparison(null);
+      assertEquals("expected:<null> but was:<a>", failure);
+    }
+
+    public void testBug609972() {
+      String failure = new ComparisonCompactor(10, "S&P500", "0").formatCompactedComparison(null);
+      assertEquals("expected:<[S&P50]0> but was:<[]0>", failure);
+    }
+
+}
